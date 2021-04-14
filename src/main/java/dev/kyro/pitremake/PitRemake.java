dd682a63487d02ab28988bd6d1703f0422d33e4d
KyroKrypt <kyrokrypt@gmail.com>
2021-04-13 19:03:41 -0700

    Setup

--- src/main/java/dev/kyro/pitremake/PitRemake.java
+++ src/main/java/dev/kyro/pitremake/PitRemake.java
@@ -1,2 +1,44 @@
-package dev.kyro.pitremake;public class PitRemake {
+package dev.kyro.pitremake;
+
+import dev.kyro.arcticapi.ArcticAPI;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PitRemake extends JavaPlugin {
+
+	public static PitRemake INSTANCE;
+
+	@Override
+	public void onEnable() {
+
+		INSTANCE = this;
+
+		loadConfig();
+
+		ArcticAPI.configInit(this, "prefix", "error-prefix");
+
+		registerCommands();
+		registerListeners();
+	}
+
+	@Override
+	public void onDisable() {
+
+
+	}
+
+	private void loadConfig() {
+
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+	}
+
+	private void registerCommands() {
+
+//        getCommand("printer").setExecutor(new PrinterCommand());
+	}
+
+	private void registerListeners() {
+
+//        getServer().getPluginManager().registerEvents(new PrinterEvents(), this);
+	}
 }
