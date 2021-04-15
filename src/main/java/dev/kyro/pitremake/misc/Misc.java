84bfa6cc8f890a919b91f77a107eec829885ecca
KyroKrypt <kyrokrypt@gmail.com>
2021-04-15 10:43:51 -0700

    Moved ordinalWords method

--- src/main/java/dev/kyro/pitremake/misc/Misc.java
+++ src/main/java/dev/kyro/pitremake/misc/Misc.java
@@ -16,4 +16,20 @@ public class Misc {
 
 		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2) + Math.pow(z1 - z2, 2));
 	}
+	public static String ordinalWords(int num) {
+
+		switch(num) {
+			case 1:
+				return "";
+			case 2:
+				return " second";
+			case 3:
+				return " third";
+			case 4:
+				return " fourth";
+			case 5:
+				return " fifth";
+		}
+		return "";
+	}
 }
