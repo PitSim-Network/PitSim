4ed26ce012735cd7988b1588d8b33e90001433d8
KyroKrypt <kyrokrypt@gmail.com>
2021-04-30 12:26:33 -0700

    Added prick enchant

---
+++ src/main/java/dev/kyro/pitremake/enchants/Prick.java
@@ -0,0 +1,38 @@
+package dev.kyro.pitremake.enchants;
+
+import dev.kyro.arcticapi.builders.ALoreBuilder;
+import dev.kyro.pitremake.controllers.DamageEvent;
+import dev.kyro.pitremake.controllers.PitEnchant;
+import dev.kyro.pitremake.enums.ApplyType;
+
+import java.util.List;
+
+public class Prick extends PitEnchant {
+
+	public Prick() {
+		super("Prick", false, ApplyType.PANTS,
+				"prick", "thorns");
+	}
+
+	@Override
+	public DamageEvent onDamage(DamageEvent damageEvent) {
+
+		int enchantLvl = damageEvent.getEnchantLevel(this);
+		if(enchantLvl == 0) return damageEvent;
+
+		damageEvent.selfTrueDamage += getDamage(enchantLvl);
+
+		return damageEvent;
+	}
+
+	@Override
+	public List<String> getDescription(int enchantLvl) {
+
+		return new ALoreBuilder("&7Enemies hitting you receive", "&c" + getDamage(enchantLvl) + "\u2764 &7 true damage").getLore();
+	}
+
+	public double getDamage(int enchantLvl) {
+
+		return enchantLvl * 1.25 + 1.25;
+	}
+}
