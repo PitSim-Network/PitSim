7307703e156f5a7b6f706f36884f9ad25f6aa9a3
BuildTools <unconfigured@null.spigotmc.org>
2021-05-16 01:33:57 -0700

    Explo

---
+++ PitMystics/src/main/java/dev/kyro/pitsim/enchants/Explosive.java
@@ -0,0 +1,150 @@
+package dev.kyro.pitsim.enchants;
+
+import dev.kyro.arcticapi.builders.ALoreBuilder;
+import dev.kyro.arcticapi.misc.AUtil;
+import dev.kyro.pitsim.controllers.*;
+import dev.kyro.pitsim.enums.ApplyType;
+import dev.kyro.pitsim.events.AttackEvent;
+import dev.kyro.pitsim.misc.Misc;
+import org.bukkit.Effect;
+import org.bukkit.Sound;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.util.Vector;
+
+import java.util.List;
+
+public class Explosive extends PitEnchant {
+
+	public Explosive() {
+		super("Explosive", true, ApplyType.BOWS,
+				"explosive", "explo", "ex", "explode");
+		isUncommonEnchant = true;
+	}
+
+	@EventHandler
+	public void onAttack(AttackEvent.Apply attackEvent) {
+		if(!canAttack(attackEvent)) return;
+
+		int enchantLvl = attackEvent.getAttackerEnchantLevel(this);
+		if(enchantLvl == 0) return;
+
+	}
+
+	@EventHandler
+	public void onShoot(ProjectileHitEvent event) {
+
+		if(!(event.getEntity() instanceof Arrow || event.getEntity().getShooter() instanceof Player)) return;
+
+
+		Arrow arrow = (Arrow) event.getEntity();
+		Player shooter = (Player) arrow.getShooter();
+		int enchantLvl = EnchantManager.getEnchantLevel(shooter, this);
+
+		Cooldown cooldown = getCooldown(shooter, getCooldown(enchantLvl));
+		if(cooldown.isOnCooldown()) return; else cooldown.reset();
+
+		for (Entity entity : arrow.getNearbyEntities(getRange(enchantLvl),
+				getRange(enchantLvl), getRange(enchantLvl))) {
+			if(entity instanceof Player) {
+				Player player = (Player) entity;
+
+				if(player != shooter) {
+					Vector force = player.getLocation().toVector().subtract(arrow.getLocation().toVector())
+							.normalize().multiply(1.25);
+					force.setY(.85f);
+
+					player.setVelocity(force);
+				}
+			}
+		}
+
+		arrow.getWorld().playSound(arrow.getLocation(), Sound.EXPLODE, 0.75f,
+				getPitch(enchantLvl));
+		arrow.getWorld().playEffect(arrow.getLocation(), getEffect(enchantLvl),
+				getEffect(enchantLvl).getData(), 100);
+
+	}
+
+	@Override
+	public List<String> getDescription(int enchantLvl) {
+
+
+		if(enchantLvl == 1) {
+			return new ALoreBuilder("&7Arrows fo POP! (" + getCooldown(enchantLvl) / 20 + "s cooldown)").getLore();
+		} else {
+			return new ALoreBuilder("&7Arrows fo BOOM! (" + getCooldown(enchantLvl) / 20 + "s cooldown)").getLore();
+		}
+
+	}
+
+	public double getRange(int enchantLvl) {
+
+		switch(enchantLvl) {
+			case 1:
+				return 1;
+			case 2:
+				return 2.5;
+			case 3:
+				return 6;
+
+		}
+		return 0;
+	}
+
+	public float getPitch(int enchantLvl) {
+
+		switch(enchantLvl) {
+			case 1:
+				return 2;
+			case 2:
+				return 1;
+			case 3:
+				return 1.4F;
+
+		}
+		return 0;
+	}
+
+	public Effect getEffect(int enchantLvl) {
+
+		switch(enchantLvl) {
+			case 1:
+				return Effect.EXPLOSION_LARGE;
+			case 2:
+				return Effect.EXPLOSION_HUGE;
+			case 3:
+				return Effect.EXPLOSION_HUGE;
+
+		}
+		return null;
+	}
+
+	public int getCooldown(int enchantLvl) {
+
+		switch(enchantLvl) {
+			case 1:
+				return 100;
+			case 2:
+				return 60;
+			case 3:
+				return 100;
+
+		}
+		return 0;
+	}
+
+	public int getSpeedAmplifier(int enchantLvl) {
+
+		return Misc.linearEnchant(enchantLvl, 0.5, 1);
+	}
+
+	public int getSpeedDuration(int enchantLvl) {
+
+		return enchantLvl * 2 + 1;
+	}
+}
