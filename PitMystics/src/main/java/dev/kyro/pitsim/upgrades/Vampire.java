92387dbc872fc5ab3391300906c8d8f634cc41cf
KyroKrypt <kyrokrypt@gmail.com>
2021-05-28 09:55:13 -0700

    regeneration on kill

--- PitMystics/src/main/java/dev/kyro/pitsim/upgrades/Vampire.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/upgrades/Vampire.java
@@ -3,9 +3,12 @@ package dev.kyro.pitsim.upgrades;
 import dev.kyro.arcticapi.builders.ALoreBuilder;
 import dev.kyro.pitsim.controllers.PitUpgrade;
 import dev.kyro.pitsim.events.AttackEvent;
+import dev.kyro.pitsim.events.KillEvent;
+import dev.kyro.pitsim.misc.Misc;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.List;
 
@@ -17,12 +20,18 @@ public class Vampire extends PitUpgrade {
 
 	@EventHandler
 	public void onAttack(AttackEvent.Apply attackEvent) {
-
 		if(!playerHasUpgrade(attackEvent.attacker)) return;
 
 		int healing = 1;
 		if(attackEvent.arrow != null && attackEvent.arrow.isCritical()) healing = 3;
 		attackEvent.attacker.setHealth(Math.min(attackEvent.attacker.getHealth() + healing, attackEvent.attacker.getMaxHealth()));
+	}
+
+	@EventHandler
+	public void onKill(KillEvent killEvent) {
+		if(!playerHasUpgrade(killEvent.killer)) return;
+
+		Misc.applyPotionEffect(killEvent.killer, PotionEffectType.REGENERATION, 160, 0, true, false);
 	}
 
 	@Override
