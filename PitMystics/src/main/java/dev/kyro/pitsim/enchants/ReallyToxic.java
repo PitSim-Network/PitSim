07e717bf58ff7da9e9afb229a5bf5fd9ea1498ec
KyroKrypt <kyrokrypt@gmail.com>
2021-08-01 23:47:51 -0700

    Buff ReallyToxic

--- PitMystics/src/main/java/dev/kyro/pitsim/enchants/ReallyToxic.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/enchants/ReallyToxic.java
@@ -2,13 +2,18 @@ package dev.kyro.pitsim.enchants;
 
 import dev.kyro.arcticapi.builders.ALoreBuilder;
 import dev.kyro.arcticapi.misc.AOutput;
+import dev.kyro.arcticapi.misc.ASound;
 import dev.kyro.pitsim.PitSim;
 import dev.kyro.pitsim.controllers.HitCounter;
 import dev.kyro.pitsim.controllers.objects.PitEnchant;
 import dev.kyro.pitsim.enums.ApplyType;
 import dev.kyro.pitsim.events.AttackEvent;
 import dev.kyro.pitsim.events.HealEvent;
+import dev.kyro.pitsim.events.VolleyShootEvent;
 import dev.kyro.pitsim.misc.Misc;
+import javafx.beans.property.SimpleDoubleProperty;
+import org.bukkit.Sound;
+import org.bukkit.entity.Arrow;
 import org.bukkit.event.EventHandler;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -20,8 +25,9 @@ public class ReallyToxic extends PitEnchant {
 	public static List<UUID> toxicNotifCooldown = new ArrayList<>();
 
 	public ReallyToxic() {
-		super("Really Toxic", false, ApplyType.NONE,
+		super("Really Toxic", false, ApplyType.PANTS,
 				"reallytoxic", "really-toxic", "toxic", "rt");
+		isUncommonEnchant = true;
 	}
 
 	@EventHandler
@@ -29,7 +35,7 @@ public class ReallyToxic extends PitEnchant {
 		int charge = HitCounter.getCharge(event.player, this);
 		if(charge == 0) return;
 
-		int reduction = Math.min(charge, 40);
+		int reduction = Math.min(charge, getMaxReduction());
 		event.multipliers.add(Misc.getReductionMultiplier(reduction));
 	}
 
@@ -38,7 +44,7 @@ public class ReallyToxic extends PitEnchant {
 
 		int attackerCharge = HitCounter.getCharge(attackEvent.attacker, this);
 		if(attackerCharge != 0 && !toxicNotifCooldown.contains(attackEvent.attacker.getUniqueId())) {
-			AOutput.send(attackEvent.attacker, "&a&lTOXIC!&f You heal &a" + Math.min(attackerCharge, 40) + "% &aless from enchants");
+			AOutput.send(attackEvent.attacker, "&a&lTOXIC!&f You heal &a" + Math.min(attackerCharge, getMaxReduction()) + "% &aless from enchants");
 			toxicNotifCooldown.add(attackEvent.attacker.getUniqueId());
 			new BukkitRunnable() {
 				@Override
@@ -49,11 +55,18 @@ public class ReallyToxic extends PitEnchant {
 		}
 
 		if(!canApply(attackEvent)) return;
+		if(attackEvent.event.getDamager() instanceof Arrow) {
+
+		}
 
 		int enchantLvl = attackEvent.getAttackerEnchantLevel(this);
 		if(enchantLvl == 0) return;
 
 		int charge = HitCounter.getCharge(attackEvent.defender, this);
+		if(charge == 0) {
+			ASound.play(attackEvent.attacker, Sound.SPIDER_IDLE, 1, 1);
+			ASound.play(attackEvent.defender, Sound.SPIDER_IDLE, 1, 1);
+		}
 		HitCounter.setCharge(attackEvent.defender, this, charge + getReductionPerHit(enchantLvl));
 
 		PitEnchant thisEnchant = this;
@@ -63,18 +76,28 @@ public class ReallyToxic extends PitEnchant {
 				int charge = HitCounter.getCharge(attackEvent.defender, thisEnchant);
 				HitCounter.setCharge(attackEvent.defender, thisEnchant, charge - getReductionPerHit(enchantLvl));
 			}
-		}.runTaskLater(PitSim.INSTANCE, 200L);
+		}.runTaskLater(PitSim.INSTANCE, getStackTime() * 20);
 	}
 
 	@Override
 	public List<String> getDescription(int enchantLvl) {
 
-		return new ALoreBuilder("&7Strikes apply 1 stack of", "&aToxicity&7. Stacks last &410s", "&7and reduce all healing by",
-				"&a" + getReductionPerHit(enchantLvl) + "%&7, up to &a40%").getLore();
+		return new ALoreBuilder("&7Strikes apply 1 stack of", "&aToxicity&7. Stacks last &4" + getStackTime() + "s", "&7and reduce all healing by",
+				"&a" + getReductionPerHit(enchantLvl) + "%&7, up to &a" + getMaxReduction() + "%").getLore();
 	}
 
 	public int getReductionPerHit(int enchantLvl) {
 
 		return enchantLvl;
+	}
+
+	public int getMaxReduction() {
+
+		return 50;
+	}
+
+	public long getStackTime() {
+
+		return 12;
 	}
 }
