6af8bc188f4736d7c09e494e2c5a4c476d31dfff
KyroKrypt <kyrokrypt@gmail.com>
2021-09-19 02:44:25 -0700

    Added simple captcha system

--- PitMystics/src/main/java/dev/kyro/pitsim/inventories/CaptchaPanel.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/inventories/CaptchaPanel.java
@@ -12,60 +12,60 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class CaptchaPanel extends AGUIPanel {
-    public CaptchaGUI captchaGUI;
+	public CaptchaGUI captchaGUI;
 
-    public int slot;
-    public boolean completed = false;
+	public int slot;
+	public boolean completed = false;
 
-    public CaptchaPanel(AGUI gui) {
-        super(gui);
-        captchaGUI = (CaptchaGUI) gui;
+	public CaptchaPanel(AGUI gui) {
+		super(gui);
+		captchaGUI = (CaptchaGUI) gui;
 
-        slot = (int) (Math.random() * 27);
-        for(int i = 0; i < 27; i++) {
-            getInventory().setItem(i, new ItemStack(Material.STAINED_GLASS_PANE));
-        }
-        getInventory().setItem(slot, new ItemStack(Material.EYE_OF_ENDER));
-    }
+		slot = (int) (Math.random() * 27);
+		for(int i = 0; i < 27; i++) {
+			getInventory().setItem(i, new ItemStack(Material.STAINED_GLASS_PANE));
+		}
+		getInventory().setItem(slot, new ItemStack(Material.EYE_OF_ENDER));
+	}
 
-    @Override
-    public String getName() {
-        return "Click the thing that stands out";
-    }
+	@Override
+	public String getName() {
+		return "Click the thing that stands out";
+	}
 
-    @Override
-    public int getRows() {
-        return 3;
-    }
+	@Override
+	public int getRows() {
+		return 3;
+	}
 
-    @Override
-    public void onClick(InventoryClickEvent event) {
+	@Override
+	public void onClick(InventoryClickEvent event) {
 
-        int slot = event.getSlot();
-        if(event.getClickedInventory().getHolder() == this) {
+		int slot = event.getSlot();
+		if(event.getClickedInventory().getHolder() == this) {
 
-            if(slot == this.slot) {
-                completed = true;
-                player.closeInventory();
-                PlayerManager.passedCaptcha.add(player.getUniqueId());
-            }
-        }
-    }
+			if(slot == this.slot) {
+				completed = true;
+				player.closeInventory();
+				PlayerManager.passedCaptcha.add(player.getUniqueId());
+			}
+		}
+	}
 
-    @Override
-    public void onOpen(InventoryOpenEvent event) {
+	@Override
+	public void onOpen(InventoryOpenEvent event) {
 
-    }
+	}
 
-    @Override
-    public void onClose(InventoryCloseEvent event) {
-        if(completed) return;
+	@Override
+	public void onClose(InventoryCloseEvent event) {
+		if(completed) return;
 
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                new CaptchaGUI(player).open();
-            }
-        }.runTaskLater(PitSim.INSTANCE, 1L);
-    }
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				new CaptchaGUI(player).open();
+			}
+		}.runTaskLater(PitSim.INSTANCE, 1L);
+	}
 }
