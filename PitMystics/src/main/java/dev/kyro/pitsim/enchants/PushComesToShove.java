4dd2aa50492f83086a524ec9759b526610d6d477
KyroKrypt <kyrokrypt@gmail.com>
2021-10-19 20:23:02 -0700

    pcts fix

--- PitMystics/src/main/java/dev/kyro/pitsim/enchants/PushComesToShove.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/enchants/PushComesToShove.java
@@ -2,7 +2,6 @@ package dev.kyro.pitsim.enchants;
 
 import dev.kyro.arcticapi.builders.ALoreBuilder;
 import dev.kyro.arcticapi.misc.AUtil;
-import dev.kyro.pitsim.PitSim;
 import dev.kyro.pitsim.controllers.Cooldown;
 import dev.kyro.pitsim.controllers.HitCounter;
 import dev.kyro.pitsim.controllers.objects.PitEnchant;
@@ -11,7 +10,6 @@ import dev.kyro.pitsim.enums.ApplyType;
 import dev.kyro.pitsim.events.AttackEvent;
 import dev.kyro.pitsim.killstreaks.Uberstreak;
 import org.bukkit.event.EventHandler;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import java.util.List;
@@ -35,36 +33,23 @@ public class PushComesToShove extends PitEnchant {
 		PitPlayer pitDefender = PitPlayer.getPitPlayer(attackEvent.defender);
 
 		HitCounter.incrementCounter(pitPlayer.player, this);
+		if(!HitCounter.hasReachedThreshold(pitPlayer.player, this, 3)) return;
 
 		Cooldown cooldown = getCooldown(attackEvent.attacker, 200);
 		if(cooldown.isOnCooldown()) return; else cooldown.reset();
-
-		if(!HitCounter.hasReachedThreshold(pitPlayer.player, this, 3)) return;
 
 		if(pitDefender.megastreak.getClass() == Uberstreak.class && pitDefender.megastreak.isOnMega()) return;
 		Vector velocity = attackEvent.arrow.getVelocity().normalize().multiply(getPunchMultiplier(enchantLvl) / 2.35);
 		velocity.setY(0);
 
-//		if(BypassManager.bypassPCTS.contains(attackEvent.defender)) {
-//			BypassManager.bypassPCTS.remove(attackEvent.defender);
-//		}
-//		BypassManager.bypassPCTS.add(attackEvent.defender);
-
 		attackEvent.defender.setVelocity(velocity);
-
-//		new BukkitRunnable() {
-//			@Override
-//			public void run() {
-//				BypassManager.bypassPCTS.remove(attackEvent.defender);
-//			}
-//		}.runTaskLater(PitSim.INSTANCE, 40L);
 	}
 
 	@Override
 	public List<String> getDescription(int enchantLvl) {
 
 		return new ALoreBuilder("&7Every 3rd shot on a player has",
-				"&bPunch " + AUtil.toRoman(getPunchLevel(enchantLvl))).getLore();
+				"&bPunch " + AUtil.toRoman(getPunchLevel(enchantLvl)) + " &7(5s cooldown)").getLore();
 	}
 
 	public int getPunchMultiplier(int enchantLvl) {
