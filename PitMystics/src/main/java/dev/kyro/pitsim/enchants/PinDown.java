219dbb82498f10b116d43f944b383aef76991fdd
KyroKrypt <kyrokrypt@gmail.com>
2021-10-24 01:49:35 -0700

    clean up

--- PitMystics/src/main/java/dev/kyro/pitsim/enchants/PinDown.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/enchants/PinDown.java
@@ -4,7 +4,6 @@ import dev.kyro.arcticapi.builders.ALoreBuilder;
 import dev.kyro.arcticapi.misc.AOutput;
 import dev.kyro.pitsim.controllers.Cooldown;
 import dev.kyro.pitsim.controllers.objects.PitEnchant;
-import dev.kyro.pitsim.controllers.objects.PitPlayer;
 import dev.kyro.pitsim.enums.ApplyType;
 import dev.kyro.pitsim.events.AttackEvent;
 import me.clip.placeholderapi.PlaceholderAPI;
@@ -35,22 +34,13 @@ public class PinDown extends PitEnchant {
 		if(attackEvent.attacker == attackEvent.defender) return;
 		if(!attackEvent.arrow.isCritical()) return;
 
-		PitPlayer pitDefender = PitPlayer.getPitPlayer(attackEvent.defender);
-//		if(pitDefender.megastreak.getClass() == Overdrive.class && pitDefender.megastreak.isOnMega()) {
-//			String errorMessage = "&c&lPIN FAILURE! &7You cannot remove Speed and Jump Boost from %luckperms_prefix%%player_name%&7!";
-//			AOutput.send(attackEvent.attacker, PlaceholderAPI.setPlaceholders(attackEvent.defender, errorMessage));
-//			ASound.play(attackEvent.attacker, Sound.VILLAGER_NO, 1 ,1);
-//		} else {
-			if(attackEvent.defender.hasPotionEffect(PotionEffectType.SPEED))
-				attackEvent.defender.removePotionEffect(PotionEffectType.SPEED);
-			if(attackEvent.defender.hasPotionEffect(PotionEffectType.JUMP))
-				attackEvent.defender.removePotionEffect(PotionEffectType.JUMP);
-			attackEvent.defender.playSound(attackEvent.defender.getLocation(), Sound.BURP, 1, 1);
-			String pinMessage = "&c&lPINNED! &7by %luckperms_prefix%%player_name%&7. Speed and Jump Boost cancelled!";
-			String pinMessage2 = "&a&lITS A PIN! &7Removed Speed and Jump Boost from %luckperms_prefix%%player_name%&7!";
-			AOutput.send(attackEvent.defender, PlaceholderAPI.setPlaceholders(attackEvent.attacker, pinMessage));
-			AOutput.send(attackEvent.attacker, PlaceholderAPI.setPlaceholders(attackEvent.defender, pinMessage2));
-//		}
+		if(attackEvent.defender.hasPotionEffect(PotionEffectType.SPEED)) attackEvent.defender.removePotionEffect(PotionEffectType.SPEED);
+		if(attackEvent.defender.hasPotionEffect(PotionEffectType.JUMP)) attackEvent.defender.removePotionEffect(PotionEffectType.JUMP);
+		attackEvent.defender.playSound(attackEvent.defender.getLocation(), Sound.BURP, 1, 1);
+		String pinMessage = "&c&lPINNED! &7by %luckperms_prefix%%player_name%&7. Speed and Jump Boost cancelled!";
+		String pinMessage2 = "&a&lITS A PIN! &7Removed Speed and Jump Boost from %luckperms_prefix%%player_name%&7!";
+		AOutput.send(attackEvent.defender, PlaceholderAPI.setPlaceholders(attackEvent.attacker, pinMessage));
+		AOutput.send(attackEvent.attacker, PlaceholderAPI.setPlaceholders(attackEvent.defender, pinMessage2));
 	}
 
 	@Override
