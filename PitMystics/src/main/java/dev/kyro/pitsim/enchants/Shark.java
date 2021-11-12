5bb38b1bdc3b70ca0768fb3794f5166a948d9774
KyroKrypt <kyrokrypt@gmail.com>
2021-11-11 22:47:24 -0800

    add shark cap against players

--- PitMystics/src/main/java/dev/kyro/pitsim/enchants/Shark.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/enchants/Shark.java
@@ -1,6 +1,7 @@
 package dev.kyro.pitsim.enchants;
 
 import dev.kyro.arcticapi.builders.ALoreBuilder;
+import dev.kyro.pitsim.controllers.NonManager;
 import dev.kyro.pitsim.controllers.objects.PitEnchant;
 import dev.kyro.pitsim.enums.ApplyType;
 import dev.kyro.pitsim.events.AttackEvent;
@@ -31,14 +32,16 @@ public class Shark extends PitEnchant {
 			if(entity instanceof Player && ((Player) entity).getHealth() < 10) nearby++;
 		}
 
-		attackEvent.increasePercent += (getDamage(enchantLvl) / 100D) * nearby;
+		double increasePercent = (getDamage(enchantLvl) / 100D) * nearby;
+		if(NonManager.getNon(attackEvent.attacker) == null) increasePercent = Math.min(increasePercent, 0.5);
+		attackEvent.increasePercent += increasePercent;
 	}
 
 	@Override
 	public List<String> getDescription(int enchantLvl) {
 
 		return new ALoreBuilder("&7Deal &c+" + getDamage(enchantLvl) + "% &7damage per other",
-				"&7player below &c5\u2764 &7within 7", "&7blocks").getLore();
+				"&7player below &c5\u2764 &7within 7", "&7blocks (+50% max against players)").getLore();
 	}
 
 	public int getDamage(int enchantLvl) {
