8b824b252ee6a5644e88e1676b4915990f9095db
wiji <unconfigured@null.spigotmc.org>
2021-12-04 13:30:05 -0700

    Hermit Fix?

--- PitMystics/src/main/java/dev/kyro/pitsim/helmetabilities/BlobAbility.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/helmetabilities/BlobAbility.java
@@ -75,7 +75,7 @@ public class BlobAbility extends HelmetAbility {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		//test
+
 		PitBlob.blobMap.remove(player.getUniqueId());
 		if(runnable != null) runnable.cancel();
 		AOutput.send(player, "&6&lGOLDEN HELMET! &cDeactivated &9Pit Blob&c.");
