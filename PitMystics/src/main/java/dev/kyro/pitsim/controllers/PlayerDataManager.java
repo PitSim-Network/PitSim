63bd5e1b538989fece9d2fafaa3b2d6516419d05
wiji <wijigaming.yt@gmail.com>
2022-01-02 00:45:06 -0700

    NPC fix/Player data finalization

--- PitMystics/src/main/java/dev/kyro/pitsim/controllers/PlayerDataManager.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/controllers/PlayerDataManager.java
@@ -4,7 +4,10 @@ import dev.kyro.pitsim.PitSim;
 import dev.kyro.pitsim.controllers.objects.PitPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerDataManager implements Listener {
@@ -27,5 +30,23 @@ public class PlayerDataManager implements Listener {
 				}
 			}
 		}.runTaskTimer(PitSim.INSTANCE,  20L, 20);
+	}
+
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+		PitPlayer pitPlayer = PitPlayer.getPitPlayer(player);
+		pitPlayer.fullSave();
+	}
+
+	@EventHandler
+	public void onDisable(PluginDisableEvent event) {
+		if(event.getPlugin() != PitSim.INSTANCE) return;
+
+		for(Player player : Bukkit.getOnlinePlayers()) {
+			PitPlayer pitPlayer = PitPlayer.getPitPlayer(player);
+			pitPlayer.fullSave();
+		}
+
 	}
 }
