1dad42dd165a393653b8728c3a62d5211a68c39b
KyroKrypt <kyrokrypt@gmail.com>
2022-02-18 20:50:24 -0800

    display kill effects client side only

--- PitMystics/src/main/java/dev/kyro/pitsim/misc/KillEffects.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/misc/KillEffects.java
@@ -11,12 +11,10 @@ public class KillEffects {
 
 		if(killEffect == KillEffect.EXE_DEATH) {
 			Sounds.EXE.play(killer);
-			location.getWorld().playEffect(location.add(0, 1, 0), Effect.STEP_SOUND, 152);
+			killer.playEffect(location.add(0, 1, 0), Effect.STEP_SOUND, 152);
 		} else if(killEffect == KillEffect.FIRE) {
 			Sounds.KILL_FIRE.play(killer);
-			location.getWorld().spigot().playEffect(location,
-					Effect.FLAME, 0, 2, 0F, 1F, 0F, 0.08F, 100, 6);
-
+			killer.spigot().playEffect(location, Effect.FLAME, 0, 2, 0F, 1F, 0F, 0.08F, 100, 6);
 		}
 	}
 }
