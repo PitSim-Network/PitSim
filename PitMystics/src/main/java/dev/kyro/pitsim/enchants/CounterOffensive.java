2eba46bad15c795bed3f813b0aa3ea7f5ef54465
KyroKrypt <kyrokrypt@gmail.com>
2022-09-26 19:57:00 -0700

    decrease hits for co but also duration

--- PitMystics/src/main/java/dev/kyro/pitsim/enchants/CounterOffensive.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/enchants/CounterOffensive.java
@@ -39,7 +39,7 @@ public class CounterOffensive extends PitEnchant {
 	}
 
 	public int getCombo(int enchantLvl) {
-		return Math.max(6 - enchantLvl, 1);
+		return Math.max(5 - enchantLvl, 1);
 	}
 
 	public int getAmplifier(int enchantLvl) {
@@ -47,7 +47,6 @@ public class CounterOffensive extends PitEnchant {
 	}
 
 	public int getSeconds(int enchantLvl) {
-		if(enchantLvl == 1) return 2;
-		return enchantLvl * 2 - 1;
+		return Misc.linearEnchant(enchantLvl, 0.5, 2);
 	}
 }
