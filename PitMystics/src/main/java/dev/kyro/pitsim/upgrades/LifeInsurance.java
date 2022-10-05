84bda6b6643cd97c109bd4b8132b808d779b6200
KyroKrypt <kyrokrypt@gmail.com>
2022-10-05 01:41:44 -0700

    change life insurance to ensure before uber 500

--- PitMystics/src/main/java/dev/kyro/pitsim/upgrades/LifeInsurance.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/upgrades/LifeInsurance.java
@@ -17,8 +17,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LifeInsurance extends RenownUpgrade {
+	public static LifeInsurance INSTANCE;
+
 	public LifeInsurance() {
 		super("Life Insurance", "LIFE_INSURANCE", 75, 22, 12, true, 3);
+		INSTANCE = this;
 	}
 
 	@Override
@@ -34,9 +37,9 @@ public class LifeInsurance extends RenownUpgrade {
 		if(UpgradeManager.hasUpgrade(player, this)) lore.add("");
 		lore.add(ChatColor.GRAY + "Each tier:");
 		lore.add(ChatColor.translateAlternateColorCodes('&', "&7Retain mystic lives on death when on"));
-		lore.add(ChatColor.translateAlternateColorCodes('&', "&eTier I: &f550 &dUber"));
-		lore.add(ChatColor.translateAlternateColorCodes('&', "&eTier II: &f525 &dUber"));
-		lore.add(ChatColor.translateAlternateColorCodes('&', "&eTier III: &f500 &dUber"));
+		lore.add(ChatColor.translateAlternateColorCodes('&', "&eTier I: &f500 &dUber"));
+		lore.add(ChatColor.translateAlternateColorCodes('&', "&eTier II: &f450 &dUber"));
+		lore.add(ChatColor.translateAlternateColorCodes('&', "&eTier III: &f400 &dUber"));
 
 		//TODO Add functionality and decide on final values for Life Insurance
 		meta.setLore(UpgradeManager.loreBuilder(this, player, lore, isCustomPanel));
@@ -46,12 +49,12 @@ public class LifeInsurance extends RenownUpgrade {
 
 	public static boolean isApplicable(Player player) {
 		PitPlayer pitPlayer = PitPlayer.getPitPlayer(player);
-		int tier = UpgradeManager.getTier(player, "LIFE_INSURANCE");
+		int tier = UpgradeManager.getTier(player, INSTANCE.refName);
 
 //		if(pitPlayer.megastreak instanceof Overdrive && pitPlayer.getKills() >= 500 && tier >= 1) return true;
-		if(pitPlayer.megastreak instanceof Uberstreak && pitPlayer.getKills() >= 500 && tier >= 3) return true;
-		if(pitPlayer.megastreak instanceof Uberstreak && pitPlayer.getKills() >= 525 && tier >= 2) return true;
-		if(pitPlayer.megastreak instanceof Uberstreak && pitPlayer.getKills() >= 550 && tier >= 1) return true;
+		if(pitPlayer.megastreak instanceof Uberstreak && pitPlayer.getKills() >= 400 && tier >= 3) return true;
+		if(pitPlayer.megastreak instanceof Uberstreak && pitPlayer.getKills() >= 450 && tier >= 2) return true;
+		if(pitPlayer.megastreak instanceof Uberstreak && pitPlayer.getKills() >= 500 && tier >= 1) return true;
 //		if(pitPlayer.megastreak instanceof Beastmode && pitPlayer.getKills() >= 1000 && tier >= 2) return true;
 //		if(pitPlayer.megastreak instanceof Highlander && pitPlayer.getKills() >= 1000 && tier >= 2) return true;
 //		if(pitPlayer.megastreak instanceof ToTheMoon && pitPlayer.getKills() >= 1000 && tier >= 2) return true;
