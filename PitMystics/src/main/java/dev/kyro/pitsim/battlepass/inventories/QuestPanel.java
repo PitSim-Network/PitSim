898ad2345bcaec4a3acee10f78caf35b825c57e4
KyroKrypt <kyrokrypt@gmail.com>
2022-10-12 21:19:35 -0700

    fixed issue with opening panel

--- PitMystics/src/main/java/dev/kyro/pitsim/battlepass/inventories/QuestPanel.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/battlepass/inventories/QuestPanel.java
@@ -20,7 +20,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class QuestPanel extends AGUIPanel {
-	public static PassGUI passGUI;
+	public PassGUI passGUI;
 
 	public static ItemStack backItem;
 
