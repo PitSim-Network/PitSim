202734d8f6e376a81bbcd40ae194a821aca0a39a
KyroKrypt <kyrokrypt@gmail.com>
2022-10-15 13:13:54 -0700

    don't strip chat colors for ops

--- PitMystics/src/main/java/dev/kyro/pitsim/controllers/ChatManager.java
+++ PitMystics/src/main/java/dev/kyro/pitsim/controllers/ChatManager.java
@@ -19,39 +19,40 @@ public class ChatManager implements Listener {
 
 	@EventHandler
 	public void autoCorrect(AsyncPlayerChatEvent event) {
+		Player player = event.getPlayer();
 		String message = event.getMessage();
 
 		for(Player recipient : event.getRecipients()) {
 			PitPlayer recipientPlayer = PitPlayer.getPitPlayer(recipient);
 			if(recipientPlayer.playerChatDisabled) event.getRecipients().remove(recipient);
-			if(event.getPlayer().equals(recipient) && recipientPlayer.playerChatDisabled)
-				AOutput.error(event.getPlayer(), "&cYou currently have the chat muted. To disable this," +
+			if(player.equals(recipient) && recipientPlayer.playerChatDisabled)
+				AOutput.error(player, "&cYou currently have the chat muted. To disable this," +
 						"navigate to the Chat Options menu located in the &f/donator &cmenu.");
 		}
 
-		if(ItemRename.renamePlayers.containsKey(event.getPlayer())) {
-			ItemStack heldItem = ItemRename.renamePlayers.get(event.getPlayer());
+		if(ItemRename.renamePlayers.containsKey(player)) {
+			ItemStack heldItem = ItemRename.renamePlayers.get(player);
 			event.setCancelled(true);
 
 			if(Misc.isAirOrNull(heldItem)) {
-				ItemRename.renamePlayers.remove(event.getPlayer());
+				ItemRename.renamePlayers.remove(player);
 				return;
 			}
 			NBTItem nbtItem = new NBTItem(heldItem);
 			if(!nbtItem.hasKey(NBTTag.ITEM_UUID.getRef())) {
-				AOutput.error(event.getPlayer(), "&cYou can only name mystic items!");
+				AOutput.error(player, "&cYou can only name mystic items!");
 				return;
 			}
 			ItemMeta meta = heldItem.getItemMeta();
 			meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', message));
 			heldItem.setItemMeta(meta);
-			AOutput.send(event.getPlayer(), "&aSuccessfully renamed item!");
-			ItemRename.renamePlayers.remove(event.getPlayer());
+			AOutput.send(player, "&aSuccessfully renamed item!");
+			ItemRename.renamePlayers.remove(player);
 		}
 
-		message = ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', message));
-		if(ChatColorPanel.playerChatColors.containsKey(event.getPlayer()) && event.getPlayer().hasPermission("pitsim.chatcolor")) {
-			message = ChatColorPanel.playerChatColors.get(event.getPlayer()).chatColor + message;
+		if(!player.isOp()) message = ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', message));
+		if(ChatColorPanel.playerChatColors.containsKey(player) && player.hasPermission("pitsim.chatcolor")) {
+			message = ChatColorPanel.playerChatColors.get(player).chatColor + message;
 		}
 
 		message = message.replaceAll("pitsandbox", "shitsandbox")
