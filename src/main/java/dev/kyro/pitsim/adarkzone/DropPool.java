a3ce0e0e599711ad85d262b86a77d9be0c2b5fbc
KyroKrypt <kyrokrypt@gmail.com>
2023-01-02 14:19:05 -0800

    continued framework development

--- src/main/java/dev/kyro/pitsim/adarkzone/DropPool.java
+++ src/main/java/dev/kyro/pitsim/adarkzone/DropPool.java
@@ -2,16 +2,17 @@ package dev.kyro.pitsim.adarkzone;
 
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class DropPool {
+	public Map<ItemStack, Double> dropPool = new HashMap<>();
 
-	public Map<ItemStack, Double> dropPool;
-
-
-	public DropPool(Map<ItemStack, Double> dropPool) {
-		this.dropPool = dropPool;
+	public DropPool() {
 	}
 
-
+	public DropPool addItem(ItemStack item, double chance) {
+		dropPool.put(item, chance);
+		return this;
+	}
 }
