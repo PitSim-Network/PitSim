7aef1405613d1a1d9f78464340f496b28a608100
OX-S <finn.kliewer1@gmail.com>
2023-01-02 17:17:49 -0500

    addded drop pool

--- src/main/java/dev/kyro/pitsim/adarkzone/DropPool.java
+++ src/main/java/dev/kyro/pitsim/adarkzone/DropPool.java
@@ -1,4 +1,17 @@
 package dev.kyro.pitsim.adarkzone;
 
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Map;
+
 public class DropPool {
+
+	public Map<ItemStack, Double> dropPool;
+
+
+	public DropPool(Map<ItemStack, Double> dropPool) {
+		this.dropPool = dropPool;
+	}
+
+
 }
