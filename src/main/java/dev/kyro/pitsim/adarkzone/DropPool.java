075835e132ac9258364c37c05dc8c66ca4e04536
OX-S <finn.kliewer1@gmail.com>
2023-01-02 17:27:31 -0500

    added distributeRewardsMethod in DropPool.java

--- src/main/java/dev/kyro/pitsim/adarkzone/DropPool.java
+++ src/main/java/dev/kyro/pitsim/adarkzone/DropPool.java
@@ -1,5 +1,8 @@
 package dev.kyro.pitsim.adarkzone;
 
+import dev.kyro.arcticapi.misc.AUtil;
+import dev.kyro.pitsim.misc.Misc;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Map;
@@ -13,5 +16,39 @@ public class DropPool {
 		this.dropPool = dropPool;
 	}
 
+	public ItemStack getRandomDrop() {
+		return Misc.weightedRandom(dropPool);
+	}
 
+	/*
+	* Parameters: Map<Player, Double> damageMap - Map of players and their damage dealt to the boss
+	* Returns: void
+	* Description: Gives the top damage dealer 3 random drops from the drop pool, and the second place damage dealer 2
+	* random drops from the drop pool, and the third place damage dealer 1 random drop from the drop pool.
+	*/
+	public void distributeRewards(Map<Player, Double> damageMap) {
+
+		for (int j = 0; j < 3; j++) {
+			Player topDamageDealer = null;
+			double topDamage = 0;
+			for(Player player : damageMap.keySet()) {
+				double damage = damageMap.get(player);
+				if(damage > topDamage) {
+					topDamage = damage;
+					topDamageDealer = player;
+				}
+			}
+
+			if (topDamageDealer == null) return;
+
+			for(int i = j; i < 3; i++) {
+				ItemStack drop = getRandomDrop();
+				AUtil.giveItemSafely(topDamageDealer, drop);
+
+			}
+
+			damageMap.remove(topDamageDealer);
+
+		}
+	}
 }
