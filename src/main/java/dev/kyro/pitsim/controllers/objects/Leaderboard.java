2bccb5967ada5675ab10c75c144e885a842a92de
wiji <wijigaming.yt@gmail.com>
2023-01-06 11:35:10 -0700

    Added gold leaderboard

--- src/main/java/dev/kyro/pitsim/controllers/objects/Leaderboard.java
+++ src/main/java/dev/kyro/pitsim/controllers/objects/Leaderboard.java
@@ -18,8 +18,8 @@ public abstract class Leaderboard {
 	public List<LeaderboardPosition> orderedLeaderboard = new ArrayList<>();
 
 	public int slot;
-	public static List<Integer> slots = new ArrayList<>(Arrays.asList(10, 11, 13, 14, 15, 16, 20, 21, 22, 23, 24));
-	//	public static List<Integer> slots = new ArrayList<>(Arrays.asList(10, 11, 12, 13, 14, 15, 16, 20, 21, 22, 23, 24));
+//	public static List<Integer> slots = new ArrayList<>(Arrays.asList(10, 11, 13, 14, 15, 16, 20, 21, 22, 23, 24));
+		public static List<Integer> slots = new ArrayList<>(Arrays.asList(10, 11, 12, 13, 14, 15, 16, 20, 21, 22, 23, 24));
 	public String refName;
 
 	public Leaderboard(String refName) {
