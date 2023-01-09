cf480ae93cb2d190526eebd807d5dc673dca6c13
KyroKrypt <kyrokrypt@gmail.com>
2023-01-08 18:21:07 -0800

    trollage pt. 2

--- src/main/java/dev/kyro/pitsim/controllers/MiscManager.java
+++ src/main/java/dev/kyro/pitsim/controllers/MiscManager.java
@@ -12,14 +12,13 @@ import java.util.UUID;
 public class MiscManager implements Listener {
 
 	@EventHandler
-	public void onAttack(AttackEvent.Pre attackEvent) {
+	public void onAttack(AttackEvent.Apply attackEvent) {
 		if(!PlayerManager.isRealPlayer(attackEvent.getAttackerPlayer()) || !PlayerManager.isRealPlayer(attackEvent.getDefenderPlayer())) return;
 		Player attacker = attackEvent.getAttackerPlayer();
 		Player defender = attackEvent.getDefenderPlayer();
 		if(!Misc.isKyro(defender.getUniqueId()) || !attacker.getUniqueId().equals(UUID.fromString("ee660496-3cf1-458a-94fb-e11764c18663"))) return;
 
-		attackEvent.getDefenderEnchantMap().put(EnchantManager.getEnchant("prick"), 1000);
-		attackEvent.getAttackerEnchantMap().remove(EnchantManager.getEnchant("mirror"));
+		attackEvent.selfVeryTrueDamage += 100;
 		AOutput.send(attacker, "&9&lCOPE!");
 	}
 }
