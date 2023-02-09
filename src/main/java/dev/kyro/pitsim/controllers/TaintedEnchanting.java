4df7a3c1796e5cf732cf838e2cd637fe5747d0a3
wiji <wijigaming.yt@gmail.com>
2023-02-08 20:21:30 -0700

    Darkzone and new join fixes

--- src/main/java/dev/kyro/pitsim/controllers/TaintedEnchanting.java
+++ src/main/java/dev/kyro/pitsim/controllers/TaintedEnchanting.java
@@ -4,11 +4,10 @@ import de.tr7zw.nbtapi.NBTItem;
 import dev.kyro.pitsim.controllers.objects.PitEnchant;
 import dev.kyro.pitsim.enums.MysticType;
 import dev.kyro.pitsim.enums.NBTTag;
+import dev.kyro.pitsim.misc.Misc;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class TaintedEnchanting {
 	public static ItemStack enchantItem(ItemStack itemStack) {
@@ -18,28 +17,30 @@ public class TaintedEnchanting {
 		int tier = nbtItem.getInteger(NBTTag.TAINTED_TIER.getRef());
 		if(tier == 3) return null;
 
-		ItemStack returnStack;
-
-
-		double rand = Math.random();
+		ItemStack returnStack = itemStack;
 
 		if(tier == 0) {
 			int enchants;
-			if(rand <= 0.9) enchants = 1;
-			else enchants = 12;
+
+			Map<Integer, Double> enchantChance = new HashMap<>();
+			enchantChance.put(1, 0.9);
+			enchantChance.put(2, 0.1);
+			enchants = Misc.weightedRandom(enchantChance);
 
 			for(int i = 0; i < enchants; i++) {
 				try {
-					rand = Math.random();
-					int newEnchants = enchants == 1 ? 1 : (rand <= 0.7) ? 1 : 2;
 
-					EnchantManager.addEnchant(itemStack, getRandomEnchant(type, 0), newEnchants, false);
+					Map<Integer, Double> newEnchantsRandom = new HashMap<>();
+					newEnchantsRandom.put(1, 0.7);
+					newEnchantsRandom.put(2, 0.3);
+					int newEnchants = enchants == 1 ? 1 : Misc.weightedRandom(newEnchantsRandom);
+
+					returnStack = EnchantManager.addEnchant(returnStack, getRandomEnchant(type, 0), newEnchants, false);
 				} catch(Exception e) {
 					e.printStackTrace();
 				}
 			}
 		}
-
 
 		return null;
 	}
