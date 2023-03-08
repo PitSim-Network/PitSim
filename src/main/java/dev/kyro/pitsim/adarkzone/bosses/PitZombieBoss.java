9d081229812963cf2c473d6f715c855a4802257f
KyroKrypt <kyrokrypt@gmail.com>
2023-03-08 00:14:12 -0800

    tnt ability

--- src/main/java/dev/kyro/pitsim/adarkzone/bosses/PitZombieBoss.java
+++ src/main/java/dev/kyro/pitsim/adarkzone/bosses/PitZombieBoss.java
@@ -3,6 +3,7 @@ package dev.kyro.pitsim.adarkzone.bosses;
 import dev.kyro.pitsim.adarkzone.DropPool;
 import dev.kyro.pitsim.adarkzone.PitBoss;
 import dev.kyro.pitsim.adarkzone.SubLevelType;
+import dev.kyro.pitsim.adarkzone.abilities.TNTAbility;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -12,6 +13,7 @@ public class PitZombieBoss extends PitBoss {
 		super(summoner);
 
 		abilities(
+				new TNTAbility(1.0, 1)
 //				new TrueDamageAbility(4),
 //				new RuptureAbility(0.3, 25, 8, 50),
 //				new PoundAbility(0.3, 5),
