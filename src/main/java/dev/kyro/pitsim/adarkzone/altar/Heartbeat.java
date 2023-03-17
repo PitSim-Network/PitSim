a3bca82b1e5a4190d2ed20dad97c0705e0b9003b
wiji <wijigaming.yt@gmail.com>
2023-03-17 03:12:53 -0500

    Packet Test

--- src/main/java/dev/kyro/pitsim/adarkzone/altar/Heartbeat.java
+++ src/main/java/dev/kyro/pitsim/adarkzone/altar/Heartbeat.java
@@ -1,13 +1,13 @@
 package dev.kyro.pitsim.adarkzone.altar;
 
 import dev.kyro.pitsim.PitSim;
-import dev.kyro.pitsim.enums.PitEntityType;
 import dev.kyro.pitsim.misc.Misc;
+import dev.kyro.pitsim.misc.Sounds;
 import net.minecraft.server.v1_8_R3.EntityPlayer;
 import net.minecraft.server.v1_8_R3.PacketPlayOutGameStateChange;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -18,9 +18,7 @@ public class Heartbeat {
 			@Override
 			public void run() {
 				Location altarLocation = AltarManager.TEXT_LOCATION.clone();
-				for(Entity entity : altarLocation.getWorld().getNearbyEntities(altarLocation, 10, 10, 10)) {
-					if(!Misc.isEntity(entity, PitEntityType.REAL_PLAYER)) continue;
-					Player player = (Player) entity;
+				for(Player player : Bukkit.getOnlinePlayers()) {
 					EntityPlayer nmsPlayer = ((CraftPlayer) player).getHandle();
 
 					if(player.getLocation().distance(altarLocation) < AltarManager.EFFECT_RADIUS) {
@@ -29,15 +27,38 @@ public class Heartbeat {
 						nmsPlayer.playerConnection.sendPacket(new PacketPlayOutGameStateChange(7, 3f));
 
 						Misc.applyPotionEffect(player, PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 9, false, false);
-						Misc.applyPotionEffect(player, PotionEffectType.BLINDNESS, 10, 9, false, false);
+						Misc.applyPotionEffect(player, PotionEffectType.BLINDNESS, 15, 0, false, false);
+						Sounds.HEARTBEAT1.play(player.getLocation());
+
+						new BukkitRunnable() {
+							@Override
+							public void run() {
+								Sounds.HEARTBEAT2.play(player.getLocation());
+								Misc.applyPotionEffect(player, PotionEffectType.BLINDNESS, 15, 0, false, false);
+							}
+						}.runTaskLater(PitSim.INSTANCE, 8);
+
+//						new BukkitRunnable() {
+//							int count = 0;
+//
+//							@Override
+//							public void run() {
+//								if(++count >= 14) {
+//									cancel();
+//								}
+//								Misc.applyPotionEffect(player, PotionEffectType.BLINDNESS, 2, 0, false, false);
+//							}
+//						}.runTaskTimer(PitSim.INSTANCE, 23, 1);
+
 					} else {
 						nmsPlayer.playerConnection.sendPacket(new PacketPlayOutGameStateChange(2, 0));
 						nmsPlayer.playerConnection.sendPacket(new PacketPlayOutGameStateChange(8, 1000f));
 						nmsPlayer.playerConnection.sendPacket(new PacketPlayOutGameStateChange(7, 0f));
+						player.removePotionEffect(PotionEffectType.NIGHT_VISION);
 					}
 				}
 			}
-		}.runTaskTimer(PitSim.INSTANCE, 20, 20);
+		}.runTaskTimer(PitSim.INSTANCE, 0, 36);
 	}
 
 	public static void init() {
--- src/main/java/dev/kyro/pitsim/misc/Sounds.java
+++ src/main/java/dev/kyro/pitsim/misc/Sounds.java
@@ -73,6 +73,8 @@ public class Sounds {
 	public static final SoundEffect XP_GAIN = new SoundEffect(Sound.ORB_PICKUP, 1, 1);
 	public static final SoundEffect ALTAR_LEVEL_UP = new SoundEffect(Sound.WOLF_HOWL, 1, 0.8);
 	public static final SoundEffect ITEM_PICKUP = new SoundEffect(Sound.ITEM_PICKUP, 1, 1);
+	public static final SoundEffect HEARTBEAT1 = new SoundEffect(Sound.NOTE_BASS_DRUM, 1, 0.6);
+	public static final SoundEffect HEARTBEAT2 = new SoundEffect(Sound.NOTE_BASS_DRUM, 1, 0.8);
 
 	public static final SoundEffect SNAKE_ICE = new SoundEffect(Sound.GLASS, 1.3, 1.5);
 	public static final SoundEffect BONE_SNAKE = new SoundEffect(Sound.SKELETON_HURT, 1.3, 1.5);
