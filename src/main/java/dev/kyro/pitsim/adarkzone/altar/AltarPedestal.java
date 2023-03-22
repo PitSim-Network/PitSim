32968ed055cb58c1722031dad0b97487e7efaa7f
KyroKrypt <kyrokrypt@gmail.com>
2023-03-21 23:30:53 -0700

    initial altar balancing

--- src/main/java/dev/kyro/pitsim/adarkzone/altar/AltarPedestal.java
+++ src/main/java/dev/kyro/pitsim/adarkzone/altar/AltarPedestal.java
@@ -35,10 +35,8 @@ import java.util.Random;
 import java.util.UUID;
 
 public abstract class AltarPedestal implements Listener {
-
 	public static final int BASE_COST = 100;
 	public static final int WEALTH_MULTIPLIER = 2;
-
 
 	public static List<AltarPedestal> altarPedestals = new ArrayList<>();
 	public Location location;
