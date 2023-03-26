f413b88a47f5030cb36b8a9653f00f37f9df3488
KyroKrypt <kyrokrypt@gmail.com>
2023-03-26 16:26:11 -0700

    fix diamond armor lost on death

--- src/main/java/dev/kyro/pitsim/aitems/diamond/DiamondBoots.java
+++ src/main/java/dev/kyro/pitsim/aitems/diamond/DiamondBoots.java
@@ -3,7 +3,6 @@ package dev.kyro.pitsim.aitems.diamond;
 import de.tr7zw.nbtapi.NBTItem;
 import dev.kyro.arcticapi.builders.ALoreBuilder;
 import dev.kyro.pitsim.aitems.StaticPitItem;
-import dev.kyro.pitsim.aitems.TemporaryItem;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
@@ -12,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class DiamondBoots extends StaticPitItem implements TemporaryItem {
+public class DiamondBoots extends StaticPitItem {
 
 	public DiamondBoots() {
 		isShopDiamond = true;
@@ -50,10 +49,5 @@ public class DiamondBoots extends StaticPitItem implements TemporaryItem {
 	@Override
 	public boolean isLegacyItem(ItemStack itemStack, NBTItem nbtItem) {
 		return itemStack.getType() == Material.DIAMOND_BOOTS && itemStack.getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL) != 0;
-	}
-
-	@Override
-	public TemporaryType getTemporaryType() {
-		return TemporaryType.LOST_ON_DEATH;
 	}
 }
