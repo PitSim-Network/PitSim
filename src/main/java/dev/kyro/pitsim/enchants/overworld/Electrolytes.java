e2dda2d7eb45d70ced5872d75fd48d3fa4fd85d6
KyroKrypt <kyrokrypt@gmail.com>
2023-04-01 13:52:30 -0700

    fix max seconds with elec

--- src/main/java/dev/kyro/pitsim/enchants/overworld/Electrolytes.java
+++ src/main/java/dev/kyro/pitsim/enchants/overworld/Electrolytes.java
@@ -30,8 +30,9 @@ public class Electrolytes extends PitEnchant {
 			if(!potionEffect.getType().equals(PotionEffectType.SPEED)) continue;
 
 			double multiplier = potionEffect.getAmplifier() > 0 ? 0.5 : 1;
-			int ticks = (int) Math.min(potionEffect.getDuration() + (getSeconds(enchantLvl) * 20) * multiplier, getMaxSeconds() * 20);
+			int ticks = (int) (potionEffect.getDuration() + getSeconds(enchantLvl) * 20 * multiplier);
 			if(potionEffect.getAmplifier() > 0) ticks *= 0.5;
+			ticks = Math.min(ticks, getMaxSeconds() * 20);
 			Misc.applyPotionEffect(killEvent.getKiller(), PotionEffectType.SPEED, ticks,
 					potionEffect.getAmplifier(), false, false);
 			break;
