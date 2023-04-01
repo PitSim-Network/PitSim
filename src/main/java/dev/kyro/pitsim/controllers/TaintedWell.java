fa5fa3a8f62e290756337e22537e0b8cf253ed93
KyroKrypt <kyrokrypt@gmail.com>
2023-04-01 03:34:10 -0700

    remove logging

--- src/main/java/dev/kyro/pitsim/controllers/TaintedWell.java
+++ src/main/java/dev/kyro/pitsim/controllers/TaintedWell.java
@@ -540,7 +540,6 @@ public class TaintedWell implements Listener {
 	public static int getEnchantCost(int tier, Player player) {
 		int cost;
 
-		System.out.println("Tier: " + tier + " Max: " + getMaxTier(player));
 		if(tier >= getMaxTier(player)) return -1;
 
 		switch(tier + 1) {
