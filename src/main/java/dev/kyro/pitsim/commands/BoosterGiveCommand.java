0df62ea61bae181c04f8a94d639753d496220b38
KyroKrypt <kyrokrypt@gmail.com>
2023-04-12 00:16:22 -0700

    minor

--- src/main/java/dev/kyro/pitsim/commands/BoosterGiveCommand.java
+++ src/main/java/dev/kyro/pitsim/commands/BoosterGiveCommand.java
@@ -2,7 +2,6 @@ package dev.kyro.pitsim.commands;
 
 import dev.kyro.arcticapi.misc.AOutput;
 import dev.kyro.pitsim.controllers.objects.Booster;
-import dev.kyro.pitsim.controllers.objects.PitPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -18,10 +17,11 @@ public class BoosterGiveCommand implements CommandExecutor {
 		String playerString = args[0];
 		Player player = null;
 		for(Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-			if(onlinePlayer.getName().equalsIgnoreCase(playerString)) player = onlinePlayer;
+			if(!onlinePlayer.getName().equalsIgnoreCase(playerString)) continue;
+			player = onlinePlayer;
+			break;
 		}
 		if(player == null) return false;
-		PitPlayer pitPlayer = PitPlayer.getPitPlayer(player);
 		Booster.setBooster(player, args[1], Booster.getBoosterAmount(player, args[1]) + 1);
 
 		Booster booster = Booster.getBooster(args[1]);
