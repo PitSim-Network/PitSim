2d1395786d1d7ec9771137f64251c5e231b9f13e
KyroKrypt <kyrokrypt@gmail.com>
2023-04-11 19:50:19 -0700

    fix shark

--- src/main/java/dev/kyro/pitsim/enchants/overworld/Shark.java
+++ src/main/java/dev/kyro/pitsim/enchants/overworld/Shark.java
@@ -2,9 +2,12 @@ package dev.kyro.pitsim.enchants.overworld;
 
 import dev.kyro.pitsim.controllers.objects.PitEnchant;
 import dev.kyro.pitsim.enums.ApplyType;
+import dev.kyro.pitsim.enums.PitEntityType;
 import dev.kyro.pitsim.events.AttackEvent;
+import dev.kyro.pitsim.misc.Misc;
 import dev.kyro.pitsim.misc.PitLoreBuilder;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 
 import java.util.List;
@@ -23,25 +26,24 @@ public class Shark extends PitEnchant {
 		int enchantLvl = attackEvent.getAttackerEnchantLevel(this);
 		if(enchantLvl == 0) return;
 
-		List<Entity> entityList = attackEvent.getAttacker().getNearbyEntities(7, 7, 7);
 		int nearby = 0;
-
-		nearby = (int) Math.ceil(entityList.size() * 0.3);
-
-//		for(Entity entity : entityList) {
-//			if(entity instanceof Player && ((Player) entity).getHealth() < 10) nearby++;
-//		}
+		for(Entity nearbyEntity : attackEvent.getAttacker().getNearbyEntities(7, 7, 7)) {
+			if(!Misc.isEntity(nearbyEntity, PitEntityType.NON, PitEntityType.REAL_PLAYER)) continue;
+			LivingEntity livingEntity = (LivingEntity) nearbyEntity;
+			if(livingEntity.getHealth() >= 10) continue;
+			nearby++;
+		}
 
 		double increasePercent = getDamage(enchantLvl) * nearby;
-		increasePercent = Math.min(increasePercent, (getCap(enchantLvl) / 100D));
+		increasePercent = Math.min(increasePercent, getCap(enchantLvl));
 		attackEvent.increasePercent += increasePercent;
 	}
 
 	@Override
 	public List<String> getNormalDescription(int enchantLvl) {
 		return new PitLoreBuilder(
-				"&7Deal &c+" + getDamage(enchantLvl) + "% &7damage per other player below &c5\u2764 &7within 7 " +
-				"blocks (&c+" + getCap(enchantLvl) + "% &7max)"
+				"&7Deal &c+" + getDamage(enchantLvl) + "% &7damage per other player below &c" + Misc.getHearts(getHealthThreshold()) +
+				" &7within 7 " + "blocks (&c+" + getCap(enchantLvl) + "% &7max)"
 		).getLore();
 	}
 
@@ -49,6 +51,10 @@ public class Shark extends PitEnchant {
 	public String getSummary() {
 		return getDisplayName(false, true) + " &7is an enchant that does more damage " +
 				"when there are other players or bots nearby that have low health";
+	}
+
+	public double getHealthThreshold() {
+		return 10;
 	}
 
 	public int getDamage(int enchantLvl) {
