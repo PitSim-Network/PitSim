192828bd19ee6bbd16213d78f696465d109d5a4f
wiji <wijigaming.yt@gmail.com>
2023-05-12 00:28:35 -0700

    stand

--- src/main/java/dev/kyro/pitsim/commands/ATestCommand.java
+++ src/main/java/dev/kyro/pitsim/commands/ATestCommand.java
@@ -1,12 +1,14 @@
 package dev.kyro.pitsim.commands;
 
-import dev.kyro.pitsim.PitSim;
-import dev.kyro.pitsim.misc.effects.SelectiveDrop;
+import dev.kyro.pitsim.misc.CustomPitBat;
+import dev.kyro.pitsim.misc.EntityManager;
+import net.minecraft.server.v1_8_R3.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class ATestCommand implements CommandExecutor {
 
@@ -14,17 +16,53 @@ public class ATestCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(!(sender instanceof Player)) return false;
 		Player player = (Player) sender;
-		if(!player.isOp()) return false;
 
-		SelectiveDrop selectiveDrop = new SelectiveDrop(player.getInventory().getItemInHand(), player.getLocation());
-		selectiveDrop.dropItem();
+		EntityManager.registerEntity("PitBat", 65, CustomPitBat.class);
 
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				selectiveDrop.addPlayer(player);
-			}
-		}.runTaskLater(PitSim.INSTANCE, 80);
+		World nmsWorld = ((CraftWorld) player.getWorld()).getHandle();
+		EntityPlayer nmsPlayer = ((CraftPlayer) player).getHandle();
+
+		CustomPitBat bat = new CustomPitBat(nmsWorld, nmsPlayer);
+		bat.setLocation(player.getLocation().getX(), player.getLocation().getY() + 5, player.getLocation().getZ(), 0, 0);
+		nmsWorld.addEntity(bat);
+
+		MobEffect mobEffect = new MobEffect(14, Integer.MAX_VALUE, 0, false, false);
+		bat.addEffect(mobEffect);
+
+
+		EntityArmorStand armorStand = new EntityArmorStand(nmsWorld, player.getLocation().getX(), player.getLocation().getY() + 5, player.getLocation().getZ());
+		armorStand.setArms(true);
+		armorStand.setBasePlate(false);
+		armorStand.setRightArmPose(new Vector3f(0, 90, 330));
+		armorStand.setInvisible(true);
+
+		PacketPlayOutSpawnEntityLiving armorStandSpawn = new PacketPlayOutSpawnEntityLiving(armorStand);
+		nmsPlayer.playerConnection.sendPacket(armorStandSpawn);
+
+		PacketPlayOutEntityEquipment standSword = new PacketPlayOutEntityEquipment(armorStand.getId(), 0, new ItemStack(Items.DIAMOND_SWORD));
+		nmsPlayer.playerConnection.sendPacket(standSword);
+
+		PacketPlayOutAttachEntity batAttach = new PacketPlayOutAttachEntity(0, armorStand, bat);
+		nmsPlayer.playerConnection.sendPacket(batAttach);
+
+		PacketPlayOutAttachEntity attachPacket = new PacketPlayOutAttachEntity(1, bat, nmsPlayer);
+		nmsPlayer.playerConnection.sendPacket(attachPacket);
+
+//		new BukkitRunnable() {
+//			int yaw = 0;
+//
+//			@Override
+//			public void run() {
+//				PacketPlayOutEntity.PacketPlayOutRelEntityMoveLook packet = new PacketPlayOutEntity.PacketPlayOutRelEntityMoveLook(bat.getId(), (byte) 0, (byte) 0, (byte) 0, (byte) yaw, (byte) 0, false);
+//				nmsPlayer.playerConnection.sendPacket(packet);
+//
+////				PacketPlayOutEntity.PacketPlayOutRelEntityMoveLook armor = new PacketPlayOutEntity.PacketPlayOutRelEntityMoveLook(armorStand    .getId(), (byte) 0, (byte) 0, (byte) 0, (byte) yaw, (byte) 0, false);
+////				nmsPlayer.playerConnection.sendPacket(armor);
+//
+//				yaw += 15;
+//				if(yaw > 256) yaw = 0;
+//			}
+//		}.runTaskTimer(PitSim.INSTANCE, 1, 1);
 
 		return false;
 	}
---
+++ src/main/java/dev/kyro/pitsim/misc/CustomPitBat.java
@@ -0,0 +1,62 @@
+package dev.kyro.pitsim.misc;
+
+import net.minecraft.server.v1_8_R3.*;
+
+import java.lang.reflect.Field;
+
+public class CustomPitBat extends EntityBat {
+	public Entity target;
+
+	public CustomPitBat(World world, Entity target) {
+		super(world);
+		this.target = target;
+
+		setAsleep(false);
+	}
+
+	@Override
+	public void E() {
+		setTargetPos(new BlockPosition((int)target.locX, (int)target.locY + 1.0, (int)target.locZ));
+
+//		if (this.getTargetPos() != null && (!this.world.isEmpty(this.getTargetPos()) || this.getTargetPos().getY() < 1)) {
+//			this.setTargetPos(null);
+//		}
+//
+//		if (this.getTargetPos() == null || this.random.nextInt(30) == 0 || this.getTargetPos().c((int)this.locX, (int)this.locY, (int)this.locZ) < 4.0) {
+//			this.setTargetPos(new BlockPosition((int)this.locX + this.random.nextInt(7) - this.random.nextInt(7),
+//					(int)this.locY + this.random.nextInt(6) - 2, (int)this.locZ + this.random.nextInt(7) - this.random.nextInt(7)));
+//		}
+
+
+		double var3 = (double)this.getTargetPos().getX() + 0.5 - this.locX;
+		double var5 = (double)this.getTargetPos().getY() + 0.1 - this.locY;
+		double var7 = (double)this.getTargetPos().getZ() + 0.5 - this.locZ;
+		this.motX += (Math.signum(var3) * 0.5 - this.motX) * 0.10000000149011612;
+		this.motY += (Math.signum(var5) * 0.699999988079071 - this.motY) * 0.10000000149011612;
+		this.motZ += (Math.signum(var7) * 0.5 - this.motZ) * 0.10000000149011612;
+		float var9 = (float)(MathHelper.b(this.motZ, this.motX) * 180.0 / 3.1415927410125732) - 90.0F;
+		float var10 = MathHelper.g(var9 - this.yaw);
+//		this.ba = 0.5F;
+//		this.yaw = 0;
+	}
+
+	public void setTargetPos(BlockPosition var1) {
+		Field a = EntityBat.class.getDeclaredFields()[0];
+		a.setAccessible(true);
+		try {
+			a.set(this, var1);
+		} catch(IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public BlockPosition getTargetPos() {
+		Field a = EntityBat.class.getDeclaredFields()[0];
+		a.setAccessible(true);
+		try {
+			return (BlockPosition) a.get(this);
+		} catch(IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
