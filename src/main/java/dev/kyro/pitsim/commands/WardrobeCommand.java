3d1f445b58106bb87f014361623802c31c91ef77
KyroKrypt <kyrokrypt@gmail.com>
2023-05-13 19:23:56 -0700

    wardrobe and ec commands

--- src/main/java/dev/kyro/pitsim/commands/WardrobeCommand.java
+++ src/main/java/dev/kyro/pitsim/commands/WardrobeCommand.java
@@ -1,6 +1,10 @@
 package dev.kyro.pitsim.commands;
 
+import dev.kyro.arcticapi.misc.AOutput;
 import dev.kyro.pitsim.PitSim;
+import dev.kyro.pitsim.controllers.objects.PitPlayer;
+import dev.kyro.pitsim.megastreaks.StashStreaker;
+import dev.kyro.pitsim.misc.Sounds;
 import dev.kyro.pitsim.storage.EnderchestGUI;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -13,7 +17,15 @@ public class WardrobeCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(!(sender instanceof Player)) return false;
 		Player player = (Player) sender;
-		if(!player.isOp() && !PitSim.isDev()) return false;
+		PitPlayer pitPlayer = PitPlayer.getPitPlayer(player);
+
+		if(!player.isOp() && !PitSim.isDev()) {
+			if(!(pitPlayer.getMegastreak() instanceof StashStreaker) || !pitPlayer.isOnMega()) {
+				AOutput.error(player, "&c&lERROR!&7 You must be on " + StashStreaker.INSTANCE.getCapsDisplayName() + "&7 to use this!");
+				Sounds.NO.play(player);
+				return false;
+			}
+		}
 
 		EnderchestGUI enderchestGUI = new EnderchestGUI(player, player.getUniqueId());
 		enderchestGUI.enderchestPanel.openPanel(enderchestGUI.wardrobePanel);
