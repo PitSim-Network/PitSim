ffaa5863f8169817c345a2e7723711121afe8eb2
wiji <wijigaming.yt@gmail.com>
2023-05-25 17:47:19 -0700

    Ban alerts

--- src/main/java/dev/kyro/pitsim/controllers/LitebansManager.java
+++ src/main/java/dev/kyro/pitsim/controllers/LitebansManager.java
@@ -1,14 +1,11 @@
 package dev.kyro.pitsim.controllers;
 
-import dev.kyro.arcticapi.libs.discord.DiscordWebhook;
 import dev.kyro.pitsim.PitSim;
+import dev.kyro.pitsim.enums.DiscordLogChannel;
 import dev.kyro.pitsim.misc.Misc;
-import dev.kyro.pitsim.misc.PrivateInfo;
 import litebans.api.Entry;
 import litebans.api.Events;
-import org.bukkit.scheduler.BukkitRunnable;
 
-import java.io.IOException;
 import java.util.Objects;
 
 public class LitebansManager {
@@ -22,29 +19,13 @@ public class LitebansManager {
 				if(entry.getType().equals("ban")) {
 					if(!Objects.equals(entry.getExecutorName(), "Console")) return;
 
-					String message = "```ID: " + entry.getId() + "\011Player: " + entry.getRemovedByName() + "\012Reason: \012"
-							+ entry.getReason() + "\012Duration: " + entry.getDurationString() + "```";
+					String message = "```ID: " + entry.getId() + "\nPlayer: |\nReason: "
+							+ entry.getReason() + "\nDuration: " + entry.getDurationString() + "```," + entry.getUuid();
 
-					Misc.alertDiscord(message);
-					sendBan(message);
+					Misc.logToDiscord(DiscordLogChannel.BAN_LOG_CHANNEL, message);
 				}
 			}
 		});
-	}
-
-	public static void sendBan(String message) {
-		DiscordWebhook discordWebhook = new DiscordWebhook(PrivateInfo.BANS_WEBHOOK);
-		discordWebhook.setContent(message);
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				try {
-					discordWebhook.execute();
-				} catch(IOException exception) {
-					exception.printStackTrace();
-				}
-			}
-		}.runTaskAsynchronously(PitSim.INSTANCE);
 	}
 
 }
